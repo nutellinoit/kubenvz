9c67298c0a2d72b55f14007fba103db28f392fb7
Samuele Chiocca <samuele@sighup.io>
2020-01-11 16:27:34 +0100

    Fix tests

--- kubenvz.py
+++ kubenvz.py
@@ -8,7 +8,7 @@ class Parser(argparse.ArgumentParser):
         self.print_help()
         exit(1)
 
-parser = Parser(description='Manage kubectl and kustomize versions v0.1.0-rc4')
+parser = Parser(description='Manage kubectl and kustomize versions v0.1.0-rc5')
 parser.add_argument('program', choices=['kubectl', 'kustomize'], help='program')
 
 commands = parser.add_subparsers(title='commands', dest='commands')
